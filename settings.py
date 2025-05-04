0c91ac4e29c800aca880796831433d026745ccb5
claudioeduleita <claudioedu.leite@hotmail.com>
2025-05-04 16:57:50 -0300

    Primeiro commit: projeto votacao

--- settings.py
+++ settings.py
@@ -1,4 +1,15 @@
 # 3. settings.py (trechos importantes)
-AUTH_USER_MODEL = 'votacao.Usuario'
-LOGIN_URL = '/login/'
-LOGIN_REDIRECT_URL = '/votar/'
+import os
+import dj_database_url
+
+SECRET_KEY = os.getenv("SECRET_KEY", "insecure-default-key")
+DEBUG = os.getenv("DEBUG", "True") == "True"
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
